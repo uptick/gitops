4835f538e9e88e43a45ff7df731accfc1b57504e
Luke Hodkinson <furious.luke@gmail.com>
2018-07-05 15:39:32 +1000

    Adapt to new directory structure

--- gitops/cluster.py
+++ gitops/cluster.py
@@ -9,9 +9,10 @@ class Cluster:
 
     def from_path(self, path):
         self.namespaces = {}
+        path = os.path.join(path, 'apps')
         for entry in os.listdir(path):
             entry_path = os.path.join(path, entry)
-            if entry[0] != '.' and not os.path.isfile(entry_path):
+            if entry[0][0] != '.' and not os.path.isfile(entry_path):
                 ns = Namespace(entry)
                 ns.from_path(entry_path)
                 self.namespaces[entry] = ns
