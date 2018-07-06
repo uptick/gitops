7d1f9d8e899b05a04bc0f1e63aa600f578be6857
Luke Hodkinson <furious.luke@gmail.com>
2018-07-06 10:07:45 +1000

    Silly mistake

--- gitops/cluster.py
+++ gitops/cluster.py
@@ -12,7 +12,7 @@ class Cluster:
         path = os.path.join(path, 'apps')
         for entry in os.listdir(path):
             entry_path = os.path.join(path, entry)
-            if entry[0][0] != '.' and not os.path.isfile(entry_path):
+            if entry[0] != '.' and not os.path.isfile(entry_path):
                 ns = Namespace(entry)
                 ns.from_path(entry_path)
                 self.namespaces[entry] = ns
