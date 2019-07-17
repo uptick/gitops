bf7df9ea9a80c5e99d702682b0ee8a4812108b01
Dean Mckendry <dean.mckendry91@gmail.com>
2019-07-17 11:50:29 +1000

    Remove sanic dependency from gitops

--- gitops_server/main.py
+++ gitops_server/main.py
@@ -4,11 +4,30 @@ from sanic.response import json
 
 from .app import app
 from .github_webhook import github_webhook
-from .utils import error_handler
 from .worker import get_worker
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('gitops')
+
+
+
+def error_handler(view):
+    """ Decorator to handle view errors.
+
+    Catches any exceptions thrown from a view and encodes them properly. At the
+    moment we're capturing any exception and returning it as a string. This
+    should be handled more gracefully and also catch more specific errors.
+    """
+    @wraps(view)
+    async def inner(*args, **kwargs):
+        try:
+            return await view(*args, **kwargs)
+        except Exception as e:
+            return json({
+                'error': e.__class__.__name__,
+                'details': str(e)
+            }, status=400)
+    return inner
 
 
 @app.post('/webhook')
