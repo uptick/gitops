6298c27f92bdb449d4fa258c42f6c3dd4b0b4c0a
Jarek Glowacki <jarekwg@gmail.com>
2020-05-14 11:01:33 +1000

    Add TODOs

--- tasks.py
+++ tasks.py
@@ -60,8 +60,8 @@ def deploy(ctx):
         f' --set domain=develop.onuptick.com'  # TODO: This should use cluster_name, not 'develop'.
         # f" --set domain={cluster_details['name']}.onuptick.com"
         ' --set environment.GIT_CRYPT_KEY_FILE=/etc/gitops/git_crypt_key'
-        f" --set environment.CLUSTER_NAME={cluster_details['name']}"
-        f" --set secrets.ACCOUNT_ID={b64encode(str(ACCOUNT_ID).encode()).decode()}"
+        f" --set environment.CLUSTER_NAME={cluster_details['name']}"  # ---- TODO: Automatically fetch from user's current settings
+        f" --set secrets.ACCOUNT_ID={b64encode(str(ACCOUNT_ID).encode()).decode()}"  # ---- TODO: Automatically fetch from user's current settings
         f" --set secrets.SLACK_URL={get_secret('SLACK_URL')}"
         f" --set secrets.GITHUB_OAUTH_TOKEN={get_secret('GITHUB_OAUTH_TOKEN')}"
         f" --set secrets.GITHUB_WEBHOOK_KEY={get_secret('GITHUB_WEBHOOK_KEY')}"
