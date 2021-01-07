27a45c5087db32579f658744d9670a76e361dad1
william chu <williamchu@uptickhq.com>
2021-01-07 15:38:51 +1100

    Command to create proxy to database instances

--- gitops_server/app.py
+++ gitops_server/app.py
@@ -3,4 +3,4 @@ from sanic import Sanic
 # The Sanic application. This is defined in its own file so that it can be
 # imported into other modules and have hooks defined without encountering
 # cyclic imports.
-app = Sanic()
+app = Sanic("gitops")
