553b9693b59fabc0ed4c23341781ba0a3bbe8862
william chu <williamchu@uptickhq.com>
2021-05-27 17:36:08 +1000

    Removing old project tools / unnused sanic files

--- gitops_server/__main__.py
+++
@@ -1,4 +0,0 @@
-from gitops_server.main import main
-
-if __name__ == '__main__':
-    main()
--- gitops_server/app.py
+++
@@ -1,6 +0,0 @@
-from sanic import Sanic
-
-# The Sanic application. This is defined in its own file so that it can be
-# imported into other modules and have hooks defined without encountering
-# cyclic imports.
-app = Sanic("gitops")
