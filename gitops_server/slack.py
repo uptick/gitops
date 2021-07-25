ba34cf4076b32f8246445cc58b4335d40a03ea60
william chu <williamchu@uptickhq.com>
2021-07-25 16:25:51 +1000

    fix(deploy): Fix httpx post request

--- gitops_server/slack.py
+++ gitops_server/slack.py
@@ -16,7 +16,7 @@ async def post(message):
     url = os.environ["SLACK_URL"]
     data = {"text": message}
     async with httpx.AsyncClient() as client:
-        response = await client.post(url, data=data)
-        if response.status >= 300:
+        response = await client.post(url, json=data)
+        if response.status_code >= 300:
             logger.error("Failed to post a message to slack (see below):")
             logger.error(f"{message}", exc_info=True)
