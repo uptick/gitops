9776a217fc7f4ce24b8bd887a62dd05742fcf7eb
william chu <williamchu@uptickhq.com>
2021-07-31 11:58:03 +1000

    Fix tests

--- gitops/__init__.py
+++ gitops/__init__.py
@@ -8,7 +8,9 @@ __version__ = "0.7.0"
 
 
 # Checking gitops version matches cluster repo version.
-versions_path = Path(os.environ.get("GITOPS_APPS_DIRECTORY", "apps")).parent / "setup.cfg"
+versions_path = (
+    Path(os.environ.get("GITOPS_APPS_DIRECTORY", "apps")).parent.absolute() / "setup.cfg"
+)
 if versions_path.exists():
     import configparser
 
@@ -16,11 +18,12 @@ if versions_path.exists():
 
     config = configparser.RawConfigParser()
     config.read(versions_path)
-    min_gitops_version = config.get("gitops.versions", "gitops")
-    if parse_version(__version__) < parse_version(min_gitops_version):
-        print(warning("Please upgrade Gitops."), file=sys.stderr)
-        print(
-            f"Your current version {success(__version__)} is less than the clusters minimum"
-            f" requirement {success(min_gitops_version)}.",
-            file=sys.stderr,
-        )
+    if "gitops.versions" in config:
+        min_gitops_version = config.get("gitops.versions", "gitops")
+        if parse_version(__version__) < parse_version(min_gitops_version):
+            print(warning("Please upgrade Gitops."), file=sys.stderr)
+            print(
+                f"Your current version {success(__version__)} is less than the clusters minimum"
+                f" requirement {success(min_gitops_version)}.",
+                file=sys.stderr,
+            )
