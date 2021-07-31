5cff7aeb60edcc6a2413361ac6c7b5dd46c8de79
william chu <williamchu@uptickhq.com>
2021-07-31 11:27:52 +1000

    feat(gitops-cli): Add a minimum gitops version check

--- gitops/__init__.py
+++ gitops/__init__.py
@@ -1,9 +1,24 @@
-import importlib.metadata
+import os
+from pathlib import Path
 
-# When installed in server mode, gitops is not installed as a package
-try:
-    _DISTRIBUTION_METADATA = importlib.metadata.metadata("gitops")
+from .utils.cli import success, warning
 
-    __version__ = _DISTRIBUTION_METADATA["Version"]
-except importlib.metadata.PackageNotFoundError:
-    __version__ = "unknown"
+__version__ = "0.7.0"
+
+
+# Checking gitops version matches cluster repo version.
+versions_path = Path(os.environ.get("GITOPS_APPS_DIRECTORY", "apps")).parent / "setup.cfg"
+if versions_path.exists():
+    import configparser
+
+    from pkg_resources.extern.packaging.version import parse as parse_version
+
+    config = configparser.RawConfigParser()
+    config.read(versions_path)
+    min_gitops_version = config.get("gitops.versions", "gitops")
+    print(warning("Please upgrade Gitops."))
+    if parse_version(__version__) < parse_version(min_gitops_version):
+        print(
+            f"Your current version {success(__version__)} is less than the clusters minimum"
+            f" requirement {success(min_gitops_version)}."
+        )
