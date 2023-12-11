3d012339c2f47413192c2930fd4134104975ec44
Mohamed ElSayed <muhammad.eldesouky@gmail.com>
2023-12-11 11:13:21 +1100

    chore: :wrench: Updating version everywhere

--- gitops/__init__.py
+++ gitops/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from . import monkeypatches  # NOQA
 from .utils.cli import success, warning
 
-__version__ = "0.9.21"
+__version__ = "0.9.22"
 
 
 # Checking gitops version matches cluster repo version.
